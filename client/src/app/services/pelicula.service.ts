850dffaa16f41e823ee120073923fa89e590bcbc
Romen Padilla <padilla.naranja@gmail.com>
2022-03-15 18:13:38 +0100

    añado espacio

--- client/src/app/services/pelicula.service.ts
+++ client/src/app/services/pelicula.service.ts
@@ -19,6 +19,8 @@ export class PeliculaService{
  
   public async getAll(page:number): Promise<PopularMoviesResponseDTO> {
     return this.http.get<PopularMoviesResponseDTO>(`http://api.themoviedb.org/3/movie/popular?api_key=1503085c4d4109b42067460d59344777&page=${page}`).toPromise()
+
+    
     
   }
    
