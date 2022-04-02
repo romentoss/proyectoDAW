9ee5c2aa0301543519a5a243c815d508facff8fd
Tony Sanjuan <tonysanj13@gmail.com>
2022-04-02 19:23:20 +0200

    starting carrousel

---
+++ client/src/app/components/film/film.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Result } from 'src/app/models/popularMoviesResponseDTO.model';
+
+@Component({
+  selector: 'app-film',
+  templateUrl: './film.component.html',
+  styleUrls: ['./film.component.scss']
+})
+export class FilmComponent implements OnInit {
+  @Input() film!: Result;
+  
+  foto="https://image.tmdb.org/t/p/w500";
+
+  ngOnInit(): void {
+    console.log("film", this.film)
+  }
+
+  getFullImgPath(id:string|undefined){
+    if(id){ 
+      return this.foto + id;
+    }
+    return 'no foto';  
+  }
+  
+}
