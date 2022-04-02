581f42377e6e587a925991847433df0f46e0d443
Romen Padilla <padilla.naranja@gmail.com>
2022-04-02 13:59:32 +0200

    Mejorando carrusel de peliculas

--- server/controllers/controllerDb.js
+++ server/controllers/controllerDb.js
@@ -15,6 +15,50 @@ controller.list =(req,res) =>{
     })
 }
 
-
+controller.listas =(req,res) =>{
+    req.getConnection((err,conn)=>{
+        conn.query('select * from userslists inner join UsersFilmShared on userslists.UserOwnerId=UsersFilmShared.id  where  id=1', (err, userslists)=>{
+            if(err){
+                res.json(err);
+            }else{
+                console.log(userslists)
+                res.render('index.ejs',{
+                    data:userslists
+                });
+               
+            }
+        })
+    })
+}
+controller.peliculas =(req,res) =>{
+    req.getConnection((err,conn)=>{
+        conn.query('select * from UsersFilms inner join UsersLists on UsersFilms.ListOwnerId=UsersLists.ListId inner join UsersFilmShared on UsersLists.UserOwnerId=UsersFilmShared.id where id=1', (err, userfilms)=>{
+            if(err){
+                res.json(err);
+            }else{
+                console.log(userfilms)
+                res.render('index.ejs',{
+                    data:userfilms
+                });
+               
+            }
+        })
+    })
+}
+controller.usuarios =(req,res) =>{
+    req.getConnection((err,conn)=>{
+        conn.query('select * from UsersFilmShared  where id=1', (err, users)=>{
+            if(err){
+                res.json(err);
+            }else{
+                console.log(users)
+                res.render('index.ejs',{
+                    data:users
+                });
+               
+            }
+        })
+    })
+}
 
 module.exports = controller;
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,6 +4,16 @@ const router = express.Router();
 
 
 const indexController = require('../controllers/controllerDb.js');
-router.get('/', indexController.list);
+router.get('/join', indexController.list);
+
+
+const listsController = require('../controllers/controllerDb.js');
+router.get('/listas', listsController.listas);
+
+const filmsController = require('../controllers/controllerDb.js');
+router.get('/peliculas', filmsController.peliculas);
+
+const usersController = require('../controllers/controllerDb.js');
+router.get('/usuarios', usersController.usuarios);
 
 module.exports = router;
